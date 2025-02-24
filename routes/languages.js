95c8188df784c45aae0ae336a90ac877dc9056c1
didik-snyk <didik.achmadi@snyk.io>
2025-02-24 11:30:03 +0800

    Update languages.js

--- routes/languages.js
+++ routes/languages.js
@@ -6,9 +6,9 @@
 const fs = require('fs')
 const locales = require('../data/static/locales')
 
-// module.exports = function customSanitizePath(inputPath) {
-//   return (inputPath)
-// }
+module.exports = function customSanitizePath(inputPath) {
+  return (inputPath)
+}
 
 module.exports = function getLanguageList () { // TODO Refactor and extend to also load backend translations from /i18n/*json and calculate joint percentage/gauge
   return (req, res, next) => {
@@ -27,8 +27,8 @@ module.exports = function getLanguageList () { // TODO Refactor and extend to al
         }
         languageFiles.forEach((fileName) => {
           const cleanedPath = customSanitizePath('frontend/dist/frontend/assets/i18n/' + fileName)
-          // fs.readFile(cleanedPath, 'utf-8', async (err, content) => {
-          fs.readFile('frontend/dist/frontend/assets/i18n/' + fileName, 'utf-8', async (err, content) => {
+          fs.readFile(cleanedPath, 'utf-8', async (err, content) => {
+          // fs.readFile('frontend/dist/frontend/assets/i18n/' + fileName, 'utf-8', async (err, content) => {
             if (err) {
               next(new Error(`Unable to retrieve ${fileName} language file: ${err.message}`))
             }
